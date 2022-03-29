2fa6dce4c4a37ac90fb7ba88eddc0da3b91b0f1b
Aseel Dweedar <archaseel.1992@gmail.com>
2022-03-29 22:53:02 +0300

    init

---
+++ src/main/java/com/example/codefellow/CodefellowApplication.java
@@ -0,0 +1,13 @@
+package com.example.codefellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodefellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodefellowApplication.class, args);
+	}
+
+}
